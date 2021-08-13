d7b864858fc20640586b79ac90baa283cde75fc9
storezhang <storezhang@gmail.com>
2021-08-13 22:14:38 +0800

    增加关闭

---
+++ echo.go
@@ -0,0 +1,10 @@
+package web
+
+import (
+	`github.com/storezhang/echox/v2`
+)
+
+// Echo Echox的简单封装
+type Echo struct {
+	*echox.Echo
+}
--- echox.go
+++ echox.go
@@ -5,7 +5,7 @@ import (
 	`github.com/storezhang/pangu`
 )
 
-func newEcho(config *pangu.Config) (echo *echox.Echo, err error) {
+func newEcho(config *pangu.Config) (echo *Echo, err error) {
 	panguConfig := new(panguConfig)
 	if err = config.Load(panguConfig); nil != err {
 		return
@@ -17,7 +17,9 @@ func newEcho(config *pangu.Config) (echo *echox.Echo, err error) {
 	if 0 != len(server.Cros.Origins) {
 		options = append(options, echox.Cros(server.Cros.Credentials, server.Cros.Origins...))
 	}
-	echo = echox.New(options...)
+	echo = &Echo{
+		Echo: echox.New(options...),
+	}
 
 	return
 }
