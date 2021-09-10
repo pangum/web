d191725f1227cc8df27b6fa3a59c1ae49973d9db
storezhang <storezhang@gmail.com>
2021-09-10 10:27:17 +0800

    增加返回域名的方法

--- config.go
+++ config.go
@@ -2,9 +2,11 @@ package web
 
 type config struct {
 	// 绑定监听主机
-	Host string `json:"host" yaml:"host"`
+	Host string `json:"host" yaml:"host" xml:"host" toml:"host"`
 	// 绑定监听端口
-	Port int `default:"9000" json:"port" yaml:"port" validate:"required"`
+	Port int `default:"9000" json:"port" yaml:"port" xml:"port" toml:"port" validate:"required"`
+	// 域名
+	Domain string `json:"domain" yaml:"domain" xml:"domain" toml:"domain"`
 	// 跨域配置
-	Cros crosConfig `json:"cros" yaml:"cros" validate:"structonly"`
+	Cros crosConfig `json:"cros" yaml:"cros" xml:"cros" toml:"cros" validate:"structonly"`
 }
--- echox.go
+++ echox.go
@@ -6,12 +6,18 @@ import (
 )
 
 func newEcho(config *pangu.Config) (echo *Echo, err error) {
-	panguConfig := new(panguConfig)
-	if err = config.Load(panguConfig); nil != err {
+	_panguConfig := new(panguConfig)
+	if err = config.Load(_panguConfig); nil != err {
 		return
 	}
-	server := panguConfig.Http.Server
+
+	server := _panguConfig.Http.Server
 	options := echox.NewOptions(echox.Addr(server.Host, server.Port))
+
+	// 配置域名
+	if "" != server.Domain {
+		options = append(options, echox.Domain(server.Domain))
+	}
 
 	// 处理跨域
 	if 0 != len(server.Cros.Origins) {
